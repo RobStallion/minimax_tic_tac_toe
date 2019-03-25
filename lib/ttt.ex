de904b4ab1577b1512ce1ad900ac7ab5bc5f4dba
Robert Francis <robertefrancis18@gmail.com>
2019-03-25 19:07:28 +0000

    move start text to ttt module and put change func name to play terminal

--- lib/ttt.ex
+++ lib/ttt.ex
@@ -1,5 +1,18 @@
 defmodule Ttt do
-  def run do
-    Ttt.PlayTerminal.start()
+
+  def play_terminal do
+    IO.puts("""
+    You have signed up to play
+    The Reigning
+    Defending
+    Undisputed
+    Tic Tac Toe champion of this computer.
+    COMPUTER!!!!!!!!!!!!!!!!!!!
+    💪💻🤳
+
+    Let's begin
+    """)
+
+    Ttt.Terminal.pick_team()
   end
 end
